8aaa518baa7fbe978963a17bf4a6d3502068d795
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2023-10-24 23:04:17 -0300

    Adds Swap for Cost

--- instances/scripts/setup_server.sh
+++ instances/scripts/setup_server.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Setting up swap space
+sudo fallocate -l 2G /swapfile
+sudo mkswap /swapfile
+sudo chmod 600 /swapfile
+sudo swapon /swapfile
 
+
+# Configuring ssh keys
 echo -e "${extra_key}" >> /home/ubuntu/.ssh/authorized_keys;
 echo -e "${terraform_key}" >> /home/ubuntu/.ssh/authorized_keys;
 
